3b2992b28a4a0ddec82c295fe776e81dbeddf667
Thalia Webb <Thaliathenerder@gmail.com>
2025-11-17 17:21:48 +0000

    now its working

--- script.js
+++
@@ -1,6 +0,0 @@
-// 1. Find the <h1> element by its ID
-const greetingElement = document.getElementById("greeting");
-// 2. Ask the user for their name
-const userName = prompt("What is your name?");
-// 3. Change the text of the <h1> element
-greetingElement.innerHTML = "Hello, " + userName + "!";
